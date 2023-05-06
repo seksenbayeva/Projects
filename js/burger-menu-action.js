0e483645eb9f28beb1e97deecc9740f7ab6d6e2d
seksenbayeva <124716801+seksenbayeva@users.noreply.github.com>
2023-05-06 21:34:23 +0600

    Delete js directory

--- js/burger-menu-action.js
+++
@@ -1,8 +0,0 @@
-function burgerMenuButton() {
-    let menuList = document.getElementById("menu-list");
-    if (menuList.style.display === "none") {
-        menuList.style.display = "block";
-    } else {
-        menuList.style.display = "none";
-    }
-}
